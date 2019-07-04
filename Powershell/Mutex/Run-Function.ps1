6fe5b9cd3e61af8df8a9911af1dabdf2c5002c31
Wade Baglin <plebmail@gmail.com>
2019-07-04 20:29:22 +1000

    Add Mutex

---
+++ Powershell/Mutex/Run-Function.ps1
@@ -0,0 +1,13 @@
+function Example {
+  begin { 
+    Write-Output "Function begin"
+  }
+  process {
+    Write-Output "Function code"
+  }
+  end {
+    Write-Output "Function end"
+  }
+}
+
+Example
